8c0e242b3cb227116372794554da9965a17d63f4
jasminekaur2106 <jasminekaur8011522@gmail.com>
2026-08-16 12:32:15 +0530

    Initial commit

---
+++ src/screens/components/header.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def header_home():
+    logo_url="https://i.ibb.co/YTYGn5qV/logo.png"
+    st.markdown(f"""
+        <div style="display: flex; align-items: center; justify-content: center; flex-direction: column; margin-bottom: 30px; margin-top: 30px;">
+        <img src="{logo_url}" style="height: 100px;"/>
+        <h1 style="text-align: center; color: #E0E3FF">CLASS<br/>TAP</h1>
+        </div>
+        """, unsafe_allow_html=True )
